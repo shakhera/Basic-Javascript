b860c8fb91be3f00715b243bd1e772dca94e320d
shakhera <shakherakhanom024@gmail.com>
2023-08-27 21:31:11 +0600

    nested object Optional chaining

--- chain.js
+++ chain.js
@@ -5,4 +5,30 @@ const user = [
         job: 'bekar'
     }
 ]
-console.log(user[0].name);
+const userResult = user[0].name;
+// console.log(userResult);
+
+const data = {
+    id: 10001,
+    count: 500,
+    data: [
+        { id: 1, name: 'shila', job: 'leader' },
+        { id: 2, name: 'khan', job: 'bekar' }
+    ]
+}
+const dataResult = data.data[1];
+// console.log(dataResult)
+
+const public = {
+    id: 18033,
+    name: 'shakhera',
+    address: {
+        street: {
+            first: '35/A ... lane',
+            second: 'secoond floor',
+            third: 'right side'
+        }
+    }
+}
+const publicResult = public.address?.streat?.third;
+console.log(publicResult)

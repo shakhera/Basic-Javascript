d44e61580cc59fb06eb405162f6ef45c565f079e
shakhera <shakherakhanom024@gmail.com>
2023-08-28 21:29:59 +0600

    Map string array

---
+++ more-map.js
@@ -0,0 +1,9 @@
+const num = [10, 20, 30];
+const division = num.map(m => m / 5);
+console.log(division);
+
+const names = ['shifa khan', 'shakhera khanom', 'saadi khan'];
+const firstLitter = names.map(n => n[0]);
+console.log(firstLitter);
+const nameLenght = names.map(l => l.length);
+console.log(nameLenght);

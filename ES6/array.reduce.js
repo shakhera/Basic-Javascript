e949ab3381d4c15faab8dfe6bb3138cac8e694cf
shakhera <shakherakhanom024@gmail.com>
2023-08-29 23:46:37 +0600

    array.reduce

--- ES6/array.reduce.js
+++ ES6/array.reduce.js
@@ -2,3 +2,9 @@ const arry = [1, 9, 17, 22];
 
 const result = arry.reduce((previous, current) => previous + current, 0);
 console.log(result);
+
+let sum = 0;
+for (let i = 0; i < arry.length; i++) {
+    sum = sum + arry[i];
+}
+console.log(sum)

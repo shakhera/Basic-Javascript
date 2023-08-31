ea36574cc132f475ba3c4e8032da4b6e0b15ff1b
shakhera <shakherakhanom024@gmail.com>
2023-08-31 21:35:37 +0600

    load data, APIs, send data to function

---
+++ EXPLORE-API/js/fetchAPP.js
@@ -0,0 +1,12 @@
+
+function loadUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayUser(data))
+}
+
+function displayUser(userData) {
+    console.log(userData);
+}
+
+

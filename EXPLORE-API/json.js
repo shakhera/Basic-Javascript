c45d06adc1d052c9a183b265eb83ac0fb6d445c4
shakhera <shakherakhanom024@gmail.com>
2023-08-31 19:09:18 +0600

    JSON Structure, parse, stringify

---
+++ EXPLORE-API/json.js
@@ -0,0 +1,26 @@
+const user = {
+    id: 1,
+    name: 'shifa khan',
+    job: 'job less'
+}
+console.log(user);
+const stringified = JSON.stringify(user);
+console.log(stringified);
+
+const shop = {
+    owner: 'shila',
+    address: {
+        street: 'gangginar par',
+        city: 'mymensongh',
+        country: 'BD'
+    },
+    product: ['laptop', 'mouse', 'charger', 'keyboard'],
+    revinue: 4500,
+    isOpen: true,
+    inNew: false
+}
+console.log(shop);
+const shopStringified = JSON.stringify(shop);
+console.log(shopStringified);
+const parseStringified = JSON.parse(shopStringified);
+console.log(parseStringified);

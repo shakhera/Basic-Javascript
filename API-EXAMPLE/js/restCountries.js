98197704ef4349506d5bcea4075623edba71c3e5
shakhera <shakherakhanom024@gmail.com>
2023-09-02 19:32:04 +0600

    Call dynamic API, load dynamic data to display

--- API-EXAMPLE/js/restCountries.js
+++ API-EXAMPLE/js/restCountries.js
@@ -11,18 +11,36 @@ const displayCounrties = (countries) => {
     // const countryInfo = countries[0].name;
     // console.log(countryInfo);
     countries.forEach(country => {
-        console.log(country.flags.png);
-        console.log(country.flags);
+        // console.log(country.cca2);
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
         <h3>name: ${country.name.common}</h3>
         <p>Captital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
         <p>continents: ${country.continents[0]}</p>
-        <p>flags: <img src="country.flags.png" alt="country.flags.png"></p>
+        <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
+
         `;
+        // <p>flags: <img src="${country.flags.png}" alt="${country.flags.png}"></p>
         // capital thakle print korbe na thakle no capital mesg dibe;
         countryElement.appendChild(countryDiv);
-
     })
+}
+const loadCountryDetails = code => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}    `
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetails(data[0]))
+}
+const displayCountryDetails = details => {
+    console.log(details);
+    const counrtyDetails = document.getElementById('country-details');
+    counrtyDetails.innerHTML = `
+    <h3>Name: ${details.name.common}</h3>
+    <strong>Country Code: ${details.cca2}</strong>
+    <p>flags: <img src="${details.flags.png}" alt="${details.flags.png}"></p>
+
+    `
 }
